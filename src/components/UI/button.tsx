cb9ecd2312ed75793d94ed11234f0911eea552be
Mitya Kors <mitya@clarityo.com>
2025-06-23 20:44:37 +0300

    remove default button bg-color

--- src/components/UI/button.tsx
+++ src/components/UI/button.tsx
@@ -8,7 +8,7 @@ export const Button: React.FC<ButtonProps> = ({ children, onClick, style }) => (
   <div className="flex flex-col gap-2 items-center">
     <button
       style={style}
-      className={`px-6 py-2 rounded-lg text-white shadow min-h-20 text-2xl font-semibold bg-stone-900 `}
+      className={`bg-stone-900 px-6 py-2 rounded-lg text-white shadow min-h-20 text-2xl font-semibold`}
       onClick={onClick}
     >
       {children}
